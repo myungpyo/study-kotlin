834a2d09393c823d68038f700f7da527c890bd50
myungpyo.shim <itmansmp@gmail.com>
2017-07-18 17:49:55 +0900

    Add examples of Delegation

---
+++ src/com/smp/kotlin/jvm/Delegation.kt
@@ -0,0 +1,26 @@
+package com.smp.kotlin.jvm
+
+fun main(args : Array<String>) {
+
+    TripleRobotGun(RobotGunImpl()).tripleFire()
+
+}
+
+interface RobotGun {
+    fun fire()
+}
+
+class RobotGunImpl() : RobotGun {
+    override fun fire() {
+        println("Tang!")
+    }
+}
+
+class TripleRobotGun(robotGun: RobotGun) : RobotGun by robotGun {
+
+    fun tripleFire() {
+        for (i in 0..2) {
+            fire()
+        }
+    }
+}
